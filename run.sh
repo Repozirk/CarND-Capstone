11c20c079a415aca1918609aee9db75d9f8b2e03
hanbrianlee <hanbrianlee@gmail.com>
2018-08-11 23:49:36 +0000

    path fix

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd ros
 catkin_make
 source devel/setup.sh
-cd ros/src/tl_detector
+cd src/tl_detector
 unzip ssd_mobilenet_v1_coco_2017_11_17.zip
 cd ../../
 roslaunch launch/styx.launch
